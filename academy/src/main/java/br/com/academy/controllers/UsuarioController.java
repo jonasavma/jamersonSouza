7475aaba6164640b78a22d476308ba00b2e21315
Jonas Avelino <33552721+jonasavma@users.noreply.github.com>
2023-02-10 14:43:32 -0300

    Atualizar projeto

--- academy/src/main/java/br/com/academy/controllers/UsuarioController.java
+++ academy/src/main/java/br/com/academy/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.academy.model.Usuario;
+
 @Controller
 public class UsuarioController {
    
@@ -11,8 +13,17 @@ public class UsuarioController {
     public ModelAndView login() {
 	
 	ModelAndView mView = new ModelAndView();
-	
 	mView.setViewName("login/login");
+	return mView;
+	
+    }
+    
+    @GetMapping("/cadastro")
+    public ModelAndView cadastrar() {
+	
+	ModelAndView mView = new ModelAndView();
+	mView.addObject("usuarioOBJ",new Usuario());
+	mView.setViewName("login/cadastro");
 	return mView;
 	
     }
